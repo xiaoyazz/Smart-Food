dbd77ecaff6b750c13268337d2a068df21eb39d4
Xiaoya Zou <xiaoyaxyz@gmail.com>
2024-11-02 21:10:37 -0400

    Initial Commit

---
+++ SmartFood/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartFood
+//
+//  Created by Xiaoya Zou on 2024-11-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartFood/SmartFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartFoodApp.swift
+//  SmartFood
+//
+//  Created by Xiaoya Zou on 2024-11-02.
+//
+
+import SwiftUI
+
+@main
+struct SmartFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
