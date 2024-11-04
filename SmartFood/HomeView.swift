4e4ecfab3e40e3e2714e54faef76e6942d7d2134
Xiaoya Zou <xiaoyaxyz@gmail.com>
2024-11-03 23:47:06 -0500

    Implement addFood, fetchCategory, checkCategory, and addCategory

---
+++ SmartFood/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SmartFood
+//
+//  Created by Xiaoya Zou on 2024-11-03.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ SmartFood/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SmartFood
+//
+//  Created by Xiaoya Zou on 2024-11-03.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
