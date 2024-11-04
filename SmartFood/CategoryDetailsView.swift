051eede17797f209c4eb69bf03de3fe32c7864d5
Xiaoya Zou <xiaoyaxyz@gmail.com>
2024-11-04 00:58:59 -0500

    Add CategoryDetailsView to show all food items in one category

---
+++ SmartFood/CategoryDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryDetailsView.swift
+//  SmartFood
+//
+//  Created by Xiaoya Zou on 2024-11-04.
+//
+
+import SwiftUI
+
+struct CategoryDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryDetailsView()
+}
