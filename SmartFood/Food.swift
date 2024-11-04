9810f4cc7203a9fdea6eaf4ff8a0d7b1c757ba88
Xiaoya Zou <xiaoyaxyz@gmail.com>
2024-11-03 20:05:41 -0500

    Add Food struct and FoodViewModel for food crud

---
+++ SmartFood/Food.swift
@@ -0,0 +1,8 @@
+//
+//  Food.swift
+//  SmartFood
+//
+//  Created by Xiaoya Zou on 2024-11-03.
+//
+
+import Foundation
---
+++ SmartFood/FoodViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FoodViewModel.swift
+//  SmartFood
+//
+//  Created by Xiaoya Zou on 2024-11-03.
+//
+
+import Foundation
