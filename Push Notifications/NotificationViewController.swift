aaa56120685c59fd1370a9a7757d126eb885322e
Xiaoya Zou <xiaoyaxyz@gmail.com>
2024-11-05 10:37:30 -0500

    Add LocalNotification for expiry date

---
+++ Push Notifications/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  Push Notifications
+//
+//  Created by Xiaoya Zou on 2024-11-05.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
