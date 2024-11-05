12d24cc4512c68b6f0fc4294e2b5585a6c3653b2
Xiaoya Zou <xiaoyaxyz@gmail.com>
2024-11-05 00:05:46 -0500

    Add search bar for categories on HomeView()

---
+++ SmartFood/SearchResultsView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsView.swift
+//  SmartFood
+//
+//  Created by Xiaoya Zou on 2024-11-04.
+//
+
+import SwiftUI
+
+struct SearchResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchResultsView()
+}
